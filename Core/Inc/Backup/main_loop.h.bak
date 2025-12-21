7e090d22ad0f68f29a1ae2bafb92583fc03e9701
Kiran Duriseti <kiranduriseti@ucsb.edu>
2025-12-20 17:15:15 -0800

    DMA uart draft

---
+++ Core/Inc/Backup/main_loop.h.bak
@@ -0,0 +1,13 @@
+/*
+ * main_loop.h
+ *
+ *  Created on: Dec 20, 2025
+ *      Author: Kiran Duriseti
+ */
+
+#ifndef INC_MAIN_LOOP_H_
+#define INC_MAIN_LOOP_H_
+
+void main_loop(void);
+
+#endif /* INC_MAIN_LOOP_H_ */
--- Core/Src/UART_print.c
+++ Core/Src/UART_print.c
@@ -9,8 +9,67 @@
 #include "gpio.h"
 #include "stm32f4xx_hal.h"
 
-extern UART_HandleTypeDef huart2;
+#define IO_SIZE 256
+#define BufferSize 32
 
-void UART_print(char* msg){
+static volatile char inputs[IO_SIZE];
+static volatile uint8_t input_size = 0;
+
+static volatile uint8_t tx_buffer1[IO_SIZE];
+static volatile uint8_t tx_buffer2[IO_SIZE];
+
+static volatile uint8_t *active = tx_buffer1;
+static volatile uint8_t *pending = tx_buffer2;
+static volatile uint8_t pending_size = 0;
+
+static volatile uint8_t tx_busy = 0;
+
+void UART_print_blocking(char* msg){
 	HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+	HAL_Delay(1000);
+}
+
+void UART_print(char *msg){
+	if (msg = NULL) return;
+
+	__disable_irq();
+
+	if (!tx_busy){
+		sprintf((char*)active, "%s", msg);
+
+		tx_busy = 1;
+
+		__enable_irq();
+
+		HAL_UART_Transmit_DMA(&huart2, (uint8_t*)msg, strlen(msg));
+	}
+	else {
+		sprintf((char*)pending, "%s", msg);
+
+		pending_size = strlen(msg);
+
+		__enable_irq();
+	}
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
+	if (huart->Instance != USART2) return;
+
+	__disable_irq();
+
+	if (pending_size == 0) {
+		return;
+		__enable_irq();
+	}
+
+	volatile uint8_t *tmp = active;
+	active = pending;
+	pending = tmp;
+
+	uint16_t len = pending_size;
+	pending_size = 0;
+
+	__enable_irq();
+
+	HAL_UART_Transmit_DMA(&huart2, (uint8_t*)active, len);
 }
